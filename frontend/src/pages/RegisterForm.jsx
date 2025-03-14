4bd6875fa678cefba63447740ffe38e1ab41e7f8
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-14 18:09:43 +0000

    Handle inputs in register form, now have to implement handleSubmit

--- frontend/src/pages/RegisterForm.jsx
+++ frontend/src/pages/RegisterForm.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
 
 const RegisterForm =  () => {
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values) => {return {...values, [name]: value}})
+        console.log(inputs)
+    }
     return (
+        <>
         <h1>Register form</h1>
+        <form>
+            <label>username
+                <input type='text'
+                 name='username'
+                 value={inputs.username || ''}
+                 onChange={handleChange} />
+            </label>
+            <br />
+            <label>email
+                <input type='text'
+                 name='email'
+                 value={inputs.email || ''}
+                 onChange={handleChange} />
+            </label>
+            <br />
+            <label> password
+                <input type='password'
+                 name='password'
+                 value={inputs.password || ''}
+                 onChange={handleChange} />
+            </label>
+        </form>
+        </>
     )
 }
 
