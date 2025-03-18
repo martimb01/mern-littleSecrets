3d1d4ffd4745c39250baaee72870753b5654a36b
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-18 16:01:50 +0000

    built initial simple welcomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
-import RegisterForm from "./pages/registerForm";
+import RegisterForm from "./pages/RegisterForm";
 import LoginForm from "./pages/LoginForm";
 import UserDashboard from "./pages/UserDashboard";
+import WelcomePage from "./pages/WelcomePage";
 const App = () => {
     return(
+        <>
+        <WelcomePage />
         <Routes>
             <Route path = '/register' element={<RegisterForm />} />
             <Route path = '/login' element={<LoginForm />} />
             <Route path = '/dashboard' element={<UserDashboard />} />
-
         </Routes>
+        </>
     )
 }
 
---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { useNavigate } from 'react-router-dom'
+const WelcomePage = () =>{
+    const nav = useNavigate()
+
+    const handleRedirect = (event) => {
+        const redirectTo =  event.target.name
+        redirectTo === 'register' ? nav('/register') : nav('/login')
+        
+    }
+    return(
+        <>
+        <h1>Welcome to Lil Secrets!</h1>
+        <div>
+            <button onClick={handleRedirect} name="register">Register</button>
+            <button onClick={handleRedirect} name='login'>Login</button>
+        </div>
+        </>
+    )
+}
+
+export default WelcomePage
