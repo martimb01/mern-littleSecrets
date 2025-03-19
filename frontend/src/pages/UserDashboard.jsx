93f1526e4a36e87dbcfdea4d7b30e0d8bd200351
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-19 20:10:07 +0000

    Users posts now being displayed in user dashboard, initial implementation, needs refactoring

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -2,27 +2,51 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import CreatePostForm from './CreatePostForm'
 
+//Helper Functions
+//Function to get user posts
+const getUserPosts = async (setUserPosts) => {
+    try {
+        const token = localStorage.getItem('token')
+        const res = await axios.get('http://localhost:3000/post/fetchUserPosts', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        setUserPosts(res.data.posts)
+        console.log('Sucessful fetch of user posts!')
 
+
+    } catch (err) {
+        console.log('Somethings gone wrong getting the posts')
+        console.log(err.message)
+    }
+}
+
+//Function to get user details
+const getUserDetails = async (setUserDetails) => {
+    try {
+        const token = localStorage.getItem('token')
+        const res = await axios.get('http://localhost:3000/user/me', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        setUserDetails(res.data.user)
+    } catch (err) {
+        console.log('Somethings gone wrong getting the details')
+        console.log(err.message)
+    }
+}
+
+// User dashboard component
 const UserDashboard = () => {
     const [userDetails, setUserDetails] = useState(null)
+    const [userPosts, setUserPosts] = useState([])
 
     useEffect(() => {
-        const getUserDetails = async () => {
-            try {
-                const token = localStorage.getItem('token')
-                const res = await axios.get('http://localhost:3000/user/me', {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-                setUserDetails(res.data.user)
-            } catch (err) {
-                console.log('Somethings gone wrong getting the details')
-                console.log(err.message)
-            }
-        }
-        getUserDetails()
-    }, [])
+        getUserPosts(setUserPosts);
+        getUserDetails(setUserDetails)
+    }, [userPosts])
     
     return(
         <>
@@ -30,9 +54,19 @@ const UserDashboard = () => {
             <div>
             <h1>Welcome to your dashboard, {userDetails.firstName + ' ' + userDetails.lastName}</h1>
             <p>{userDetails.email + ' ' + JSON.stringify(userDetails.dateOfBirth).substring(1, 11)}</p>
-            </div>
-            ) : <h1>Loading...</h1>}
+            </div>) : <h1>Loading...</h1>}
             <CreatePostForm />
+            <br/>
+
+            {/* Test to see if posts show up, later to replace with the posts component for
+            better code separation */}
+            {userPosts ? (
+                userPosts.map((post) => (
+                    <div key={post._id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.content}</p>
+                    </div>
+                ))) : <p>Loading...</p>}
         </>
     )
 }
