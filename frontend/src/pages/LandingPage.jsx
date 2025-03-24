1dcbe5694054981dcb4b6fab900557e3f7e33ad4
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-24 17:55:07 +0000

    Building logIn navBar in landing Page, maybe will refactor it into its own component later on

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,23 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logov1.png'
 import  styles from './css/landingPageStyle.module.css'
+import LoginForm from "./LoginForm"
 const LandingPage = () =>{
     const nav = useNavigate()
+    const [input, setInput] = useState('')
 
-    const handleRedirect = (event) => {
-        const redirectTo =  event.target.name
-        redirectTo === 'register' ? nav('/register') : nav('/login')
-        
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInput({...input, [name]: value})
     }
+
     return(
         <>
-        <h1>Welcome to Lil Secrets!</h1>
-        <img src={logo} />
-        <div>
-            <button onClick={handleRedirect} name="register">Register</button>
-            <button onClick={handleRedirect} name='login'>Login</button>
+        <div className={styles.loginBar}>
+            <form className={styles.loginForm}>
+                <div className={styles.loginFormRow}>
+                    <label>Username</label>
+                    <input type="text"
+                    name='username'
+                    value={input.username || ''}
+                    onChange={handleChange}></input>
+                </div>
+
+                <div className={styles.loginFormRow}>
+                    <label>Password</label>
+                    <input type="password"
+                    name='password'
+                    value={input.password || ''}
+                    onChange={handleChange}></input>
+                </div>
+            </form>
         </div>
+        <img src={logo} />
         </>
     )
 }
