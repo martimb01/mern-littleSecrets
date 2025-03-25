10c6c26741c944ad7ed91c4e426b225158b9dd85
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-25 18:12:20 +0000

    Starting work on creating posts on a particular secret page, is gonna require refactoring of the CreatePostForm in order to work

--- frontend/src/pages/SecretPage.jsx
+++ frontend/src/pages/SecretPage.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { FaSquarePlus } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const SecretPage = () => {
     const loc = useLocation()
+    const nav = useNavigate()
     const {secretName, secretId, secretDescription} = loc.state
+
+
+    const createPost = async() => {
+        nav('/postCreation', {state: {secretId: secretId}})
+    }
+    
     return(
         <>
             <NavBar />
             {secretName && <h1>{secretName}</h1>}
             {secretId && <h1>{secretId}</h1>}
             {secretDescription && <p>{secretDescription}</p>}
+            <FaSquarePlus onClick={createPost()} />
         </>
     )
 }
